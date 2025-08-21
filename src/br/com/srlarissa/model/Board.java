fa7dcdfa10226010a690b8bb7ff2e9ba46bac742
LarIssa Soares Reis <larissasoaresreis@MacBook-Air-de-LarIssa.local>
2025-08-21 19:06:46 -0300

    feat(Board) Add Board model class

---
+++ src/br/com/srlarissa/model/Board.java
@@ -0,0 +1,13 @@
+package br.com.srlarissa.model;
+import java.util.List;
+
+public class Board {
+    private final List<List<Space>> spaces;
+    public Board(List<List<Space>> spaces) {
+        this.spaces = spaces;
+    }
+
+    public List<List<Space>> getSpaces() {
+        return spaces;
+    }
+}
