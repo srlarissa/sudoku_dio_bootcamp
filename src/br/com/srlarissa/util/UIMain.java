8d1ce8f0087c2da209b55128da49014c4d395106
LarIssa Soares Reis <larissasoaresreis@MacBook-Air-de-LarIssa.local>
2025-08-23 17:13:18 -0300

    feat(UIMain): First test using the UI

---
+++ src/br/com/srlarissa/util/UIMain.java
@@ -0,0 +1,18 @@
+package br.com.srlarissa.util;
+
+import br.com.srlarissa.ui.custom.frame.MainFrame;
+import br.com.srlarissa.ui.custom.panel.MainPanel;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class UIMain {
+    public static void main(String[] args) {
+        var dimension = new Dimension(600, 600);
+        JPanel mainPanel = new MainPanel(dimension);
+        JFrame mainFrame = new MainFrame(dimension, mainPanel);
+
+        mainFrame.revalidate();
+        mainFrame.repaint();
+    }
+}
