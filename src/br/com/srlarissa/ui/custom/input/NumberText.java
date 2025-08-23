571a6bc496e12bf43de27411b40c17b1f3f453f7
LarIssa Soares Reis <larissasoaresreis@MacBook-Air-de-LarIssa.local>
2025-08-23 17:35:41 -0300

    feat(NumberText): Add NumberText

---
+++ src/br/com/srlarissa/ui/custom/input/NumberText.java
@@ -0,0 +1,27 @@
+package br.com.srlarissa.ui.custom.input;
+
+import br.com.srlarissa.model.Space;
+
+import javax.swing.*;
+import java.awt.*;
+
+import static java.awt.Font.PLAIN;
+
+
+public class NumberText extends JTextField {
+    private final Space space;
+
+    public NumberText(final Space space){
+        this.space = space;
+        var dimension = new Dimension(50, 50);
+        this.setSize(dimension);
+        this.setPreferredSize(dimension);
+        this.setVisible(true);
+        this.setFont(new Font("Arial",PLAIN, 20));
+        this.setHorizontalAlignment(CENTER);
+        this.setDocument(new NumberTextLimit());
+        this.setEnabled(!space.isFixed());
+
+        if(space.isFixed()) this.setText(space.getCurrentValue().toString());
+    }
+}
