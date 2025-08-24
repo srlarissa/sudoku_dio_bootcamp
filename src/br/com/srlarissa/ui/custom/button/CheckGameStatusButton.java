f0e8171a94956d8e8bfb9da3ff7e45e2148d4aec
LarIssa Soares Reis <larissasoaresreis@MacBook-Air-de-LarIssa.local>
2025-08-23 22:25:59 -0300

    fix(CheckGameStatusButton): Now passing event listener as parameter

--- src/br/com/srlarissa/ui/custom/button/CheckGameStatusButton.java
+++ src/br/com/srlarissa/ui/custom/button/CheckGameStatusButton.java
@@ -1,9 +1,10 @@
 package br.com.srlarissa.ui.custom.button;
 
 import javax.swing.*;
+import java.awt.event.ActionListener;
 
 public class CheckGameStatusButton extends JButton {
-    public CheckGameStatusButton(){
+    public CheckGameStatusButton(ActionListener action){
         this.setText("VERIFICAR JOGO");
         this.addActionListener(actionListener);
     }
