735a02f8b17ad16a084b95f4f8361d659a2be7cd
LarIssa Soares Reis <larissasoaresreis@MacBook-Air-de-LarIssa.local>
2025-08-23 22:22:57 -0300

    fix(ResetButton): Now passing event listener as a parameter

--- src/br/com/srlarissa/ui/custom/button/ResetButton.java
+++ src/br/com/srlarissa/ui/custom/button/ResetButton.java
@@ -1,9 +1,10 @@
 package br.com.srlarissa.ui.custom.button;
 
 import javax.swing.*;
+import java.awt.event.ActionListener;
 
 public class ResetButton extends JButton {
-    public ResetButton(){
+    public ResetButton(ActionListener action){
         this.setText("REINICIANDO O JOGO");
         this.addActionListener(actionListener);
     }
