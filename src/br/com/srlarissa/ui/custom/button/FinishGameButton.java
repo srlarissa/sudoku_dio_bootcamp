ebfaa8fca5c53c821b2d0789bc8f6eb66088b970
LarIssa Soares Reis <larissasoaresreis@MacBook-Air-de-LarIssa.local>
2025-08-23 22:25:19 -0300

    fix(FinishGameButton): Now passing event listener as parameter

--- src/br/com/srlarissa/ui/custom/button/FinishGameButton.java
+++ src/br/com/srlarissa/ui/custom/button/FinishGameButton.java
@@ -1,9 +1,10 @@
 package br.com.srlarissa.ui.custom.button;
 
 import javax.swing.*;
+import java.awt.event.ActionListener;
 
 public class FinishGameButton extends JButton {
-    public FinishGameButton(){
+    public FinishGameButton(ActionListener action){
         this.setText("CONCLUIR JOGO");
         this.addActionListener(actionListener);
     }
